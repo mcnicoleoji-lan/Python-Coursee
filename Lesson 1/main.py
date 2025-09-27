011b0b97aff466fb00244b37ee3af234e905f33d
MercyRose123 <mcnicole.@gmail.com>
2025-09-27 19:24:52 +0200

    My first code to be commited

---
+++ Lesson 1/main.py
@@ -0,0 +1,10 @@
+#Print a Sentence      
+print("Welcome to Python Programming!")
+#Print a Number 
+print (4)
+#Next Line after print
+print("Hello Python/n")
+#Pass multiple arguments for printing 
+print ("hello,5")
+#End arguement of Print Statement 
+print("welcome to",end="*")
---
+++ Lesson 1/main2.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+Mercy = "Mercy"
+print(Mercy)
+
+#User Input 
+name = input("Oji Mercy C.N:")
+
+print("/n",name, "/nwelcome to codingal")
