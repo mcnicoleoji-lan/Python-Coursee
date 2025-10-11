8a1752ff528e8a9fc48c05ff62b9b1248d903f94
MercyRose123 <mcnicole.@gmail.com>
2025-10-11 18:52:13 +0200

    commit1

---
+++ Lesson  3/Not.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)   # True, because 10 ≠ 12
+print(b != c)   # False, because 12 = 12
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a, 'and', b, 'are different.')  
+    # prints: python and coding are different.
+
+a = 4
+b=5
+
+if(a==1) !=(b==5):
+
+print ('Hello')
