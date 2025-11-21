b7c5be688731d894e975b50c728c75b1d970ec6c
MercyRose123 <mcnicole.@gmail.com>
2025-11-21 18:39:07 +0200

    commit 1

---
+++ Lesson  3/Bye-bye.py
@@ -0,0 +1,9 @@
+valid = False
+
+while not valid:
+    try:
+        n = int(input("Enter a number: "))
+        # enter an even number
+        while n % 2 == 0:
+            print("bye")
+            valid = Tru
